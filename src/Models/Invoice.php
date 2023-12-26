68aa179670e357d6a980feec2e15f3738b6101ed
YevhenBoyco <boycojoz@gmail.com>
2023-12-26 17:35:57 +0200

    fix

--- src/Models/Invoice.php
+++ src/Models/Invoice.php
@@ -5,7 +5,8 @@ namespace parazeet\PayMaster\Models;
 class Invoice
 {
     public function __construct(
-        public ?string $paymentId,  //string	Идентификатор платежа
-        public ?string $url,    //string	Адрес страницы оплаты, на которую нужно направить пользователя
+        public ?string $paymentId,  //string Идентификатор платежа
+        public ?string $url,
+        //string Адрес страницы оплаты, на которую нужно направить пользователя
     ) {}
 }
