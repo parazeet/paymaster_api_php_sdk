0e03a830840d0f07d087df6246db87a6bb7f15b9
ionov-e <ioncurly@gmail.com>
2024-01-09 17:38:25 +0300

    Refactoring: ResponseValidator.php minor improvements

--- src/Validator/ResponseValidator.php
+++ src/Validator/ResponseValidator.php
@@ -2,103 +2,107 @@
 
 namespace parazeet\PayMaster\Validator;
 
-use parazeet\PayMaster\Validator\ValidatorContract;
-use parazeet\PayMaster\Exceptions\UnknownCodeException;
+use parazeet\PayMaster\Exceptions\ErrorContentFormatException;
+use parazeet\PayMaster\Exceptions\ErrorSystemException;
+use parazeet\PayMaster\Exceptions\ErrorUnauthorizedException;
 use parazeet\PayMaster\Exceptions\EmptyResponseException;
 
 class ResponseValidator implements ValidatorContract
 {
-    private $statuses = [
+    private array $statuses = [
         // Статусы обработки сообщения
         //Код ошибки
         'validation_error' => [
             'message' => 'Некорректный запрос (неверный формат)',
-            'class' => '\parazeet\PayMaster\Exceptions\ErrorContentFormatException'
+            'class' => ErrorContentFormatException::class
         ],
         'not_authorized' => [
             'message' => 'Недостаточно прав для выполнения операции',
-            'class' => '\parazeet\PayMaster\Exceptions\ErrorUnauthorizedException'
+            'class' => ErrorUnauthorizedException::class
         ],
         'idempotency_key_violation' => [
             'message' => 'Нарушение уникальности ключа (дублирование запроса)',
-            'class' => '\parazeet\PayMaster\Exceptions\ErrorUnauthorizedException'
+            'class' => ErrorUnauthorizedException::class
         ],
         'invalid_operation' => [
             'message' => 'Запрос отклонен',
-            'class' => '\parazeet\PayMaster\Exceptions\ErrorContentFormatException'
+            'class' => ErrorContentFormatException::class
         ],
         'payment_token_revoked' => [
             'message' => 'Платежный токен отозван',
-            'class' => '\parazeet\PayMaster\Exceptions\ErrorUnauthorizedException'
+            'class' => ErrorUnauthorizedException::class
         ],
         'payment_token_blocked' => [
             'message' => 'Платежный токен временно заблокирован',
-            'class' => '\parazeet\PayMaster\Exceptions\ErrorUnauthorizedException'
+            'class' => ErrorUnauthorizedException::class
         ],
 
         //Коды авторизации
         'TransactionDeclined' => [
             'message' => 'Авторизация отклонена',
-            'class' => '\parazeet\PayMaster\Exceptions\ErrorUnauthorizedException'
+            'class' => ErrorUnauthorizedException::class
         ],
         'IssuerUnavailable' => [
             'message' => 'Эмитент недоступен',
-            'class' => '\parazeet\PayMaster\Exceptions\ErrorUnauthorizedException'
+            'class' => ErrorUnauthorizedException::class
         ],
         'RejectedByFraud' => [
             'message' => 'Отклонено фрод-мониторингом',
-            'class' => '\parazeet\PayMaster\Exceptions\ErrorSystemException'
+            'class' => ErrorSystemException::class
         ],
         'InvalidAmount' => [
             'message' => 'Недопустимая сумма',
-            'class' => '\parazeet\PayMaster\Exceptions\ErrorContentFormatException'
+            'class' => ErrorContentFormatException::class
         ],
         'InvalidAccount' => [
             'message' => 'Неверный номер карты / карта не существует',
-            'class' => '\parazeet\PayMaster\Exceptions\ErrorContentFormatException'
+            'class' => ErrorContentFormatException::class
         ],
         'BlockedAccount' => [
             'message' => 'Карта заблокирована (утеряна)',
-            'class' => '\parazeet\PayMaster\Exceptions\ErrorContentFormatException'
+            'class' => ErrorContentFormatException::class
         ],
         'OperationNotAllowed' => [
             'message' => 'Операция недоступна',
-            'class' => '\parazeet\PayMaster\Exceptions\ErrorContentFormatException'
+            'class' => ErrorContentFormatException::class
         ],
         'InsufficientFunds' => [
             'message' => 'Недостаточно средств',
-            'class' => '\parazeet\PayMaster\Exceptions\ErrorContentFormatException'
+            'class' => ErrorContentFormatException::class
         ],
         'ExpiredAccount' => [
             'message' => 'Срок действия карты истек / неверный срок действия',
-            'class' => '\parazeet\PayMaster\Exceptions\ErrorContentFormatException'
+            'class' => ErrorContentFormatException::class
         ],
         'PaymentLimitExceeded' => [
             'message' => 'Превышен лимит на сумму',
-            'class' => '\parazeet\PayMaster\Exceptions\ErrorContentFormatException'
+            'class' => ErrorContentFormatException::class
         ],
         'PaymentCountExceeded' => [
             'message' => 'Превышено кол-во попыток',
-            'class' => '\parazeet\PayMaster\Exceptions\ErrorSystemException'
+            'class' => ErrorSystemException::class
         ],
         'CardNotEnrolled' => [
             'message' => 'Карта не подписана на 3DS',
-            'class' => '\parazeet\PayMaster\Exceptions\ErrorSystemException'
+            'class' => ErrorSystemException::class
         ],
         'ThreeDSecureFailed' => [
             'message' => '3DS аутентификация не пройдена',
-            'class' => '\parazeet\PayMaster\Exceptions\ErrorSystemException'
+            'class' => ErrorSystemException::class
         ],
         'CancelledByUser' => [
             'message' => 'Платеж отклонен пользователем',
-            'class' => '\parazeet\PayMaster\Exceptions\ErrorSystemException'
+            'class' => ErrorSystemException::class
         ],
         'PaymentExpired' => [
             'message' => 'Истек срок',
-            'class' => '\parazeet\PayMaster\Exceptions\ErrorSystemException'
+            'class' => ErrorSystemException::class
         ],
     ];
 
+    /**
+     * @throws EmptyResponseException
+     */
     public function validate(?array $response = []): void
     {
         if (empty($response)) {
@@ -112,7 +116,10 @@ class ResponseValidator implements ValidatorContract
                 $errors = implode(" ", $response['errors']);
             }
 
-            throw new $this->statuses[$response['code']]['class'](
+            /** @var class-string $exceptionClassName */
+            $exceptionClassName = $this->statuses[$response['code']]['class'];
+
+            throw new $exceptionClassName(
                 $this->statuses[$response['code']]['message'] . ': ' . $errors,
                 $response['code']
             );
